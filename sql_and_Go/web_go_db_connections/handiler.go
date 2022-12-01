129adef0f85e2dcd2b1b99f4ff7bcac8ab88d07f
FASIL <fasilkp2000@gmail.com>
2022-12-01 19:41:16 +0530

    here we learn the make connection between database to go and go to web ; and learn how to insert value to database form web

---
+++ sql_and_Go/web_go_db_connections/handiler.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	err := tpl.ExecuteTemplate(w, "signup.html", nil)
+	if err != nil {
+		fmt.Println("error while parsing file", err)
+		return
+	}
+
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+
+	// checking method is post or not if any error it redirect and returns
+
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	// parsing data from the template and error checking if any error it returns
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest),
+			http.StatusBadRequest)
+		return
+	}
+
+	// geting data to variables
+
+	uname := r.PostForm.Get("firstName")
+	upwd := r.PostForm.Get("password")
+
+	// fmt.Printf("\n\n\nu_name  :%s\n\n\npwd  :%s\n\n\n", uname, upwd)
+
+	// set inserting qurey and exicute it
+
+	in_qry := `INSERT INTO A1 (usr_name,usr_pwd) VALUES ($1,$2)`
+
+	_, err = db.Exec(in_qry, uname, upwd)
+	if err != nil {
+		log.Panicln(err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+	fmt.Println("inserted succesfully")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
+}
